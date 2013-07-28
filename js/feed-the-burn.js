9438211680d4d5321e19e61b3bb0125718bc60ff
Chris King <chris.king@netapp.com>
2013-07-28 08:11:43 -0400

    improved comment structure to not use multi-line comments, and removed legacy code from initial hacking

--- js/feed-the-burn.js
+++ js/feed-the-burn.js
@@ -1,9 +1,9 @@
-/*
-@Title = feed-the-burn.js
-@Author = Chris King
-@Description = The main JS file for feed-the-burn a soundcloud app for finding mixes for your workout.
-
-*/
+//
+// @Title = feed-the-burn.js
+// @Author = Chris King
+// @Description = The main JS file for feed-the-burn a soundcloud app for finding mixes for your workout.
+// 
+//
 
 //Add Tracks to Tracklist:
 function AddTracksToTracklist(tracks, list){
@@ -20,19 +20,19 @@ $(document).ready(function() {
         client_id: 'e30e2c947a1035c10c66b6ab5780d629'
     });
     
-    /* BPM
+    // BPM
     // Find all tracks with a bpm of 120.
     // Log them to the console, then add them to a list
-    */
+    //
     SC.get('/tracks', { bpm: { from: 120 } }, function(tracks) {
         console.log(tracks);
         AddTracksToTracklist(tracks, $('#BPMList'));
     });
 
-    /* Duration
+    // Duration
     // Find all tacks with a duration of 30 - 40 minutes
     // Then Add them to a list
-    */
+    //
     var thirty_minutes_in_milis = 1800000;
     var fourty_minutes_in_milis = 2400000;
     SC.get('/tracks', { duration: { from: thirty_minutes_in_milis, to: fourty_minutes_in_milis } }, function(tracks) {
@@ -40,28 +40,11 @@ $(document).ready(function() {
         AddTracksToTracklist(tracks, $('#DurationList'));
     });
 
-    /* Genre
+    // Genre
     // Find all tracks with a genre of "dubstep"
-    */
+    //
     SC.get('/tracks', { genre: "dubstep" }, function(tracks) {
         console.log(tracks);
         AddTracksToTracklist(tracks, $('#GenreList'));
     });
 });
-
-
-
-//Example Searching
-//The line below waits for all content to be delivered to the browser before executing.
-//Demo code for tracklist
-/*
-$(document).ready(function() {
-    var url = "http://api.soundcloud.com/users/2431310/tracks.json?client_id=e30e2c947a1035c10c66b6ab5780d629";
-    $.getJSON(url, function(tracks) {
-        $(tracks).each(function(track) {
-            console.log(this);
-            $('#TrackList').append("<li>" + this.user.username + " - " + this.title + "</li>");
-        }
-    )});
-});
-*/
